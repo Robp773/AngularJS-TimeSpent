d050e3cd5a0b7bb46d19120806b2c13296f1af0c
Robp773 <robpeterman573@gmail.com>
2018-07-04 14:54:05 -0400

    added modalForm directive + modal form functionality

--- app/home/home.js
+++ app/home/home.js
@@ -1,3 +1,31 @@
 'use strict';
 
-angular.module('root-app', []);
+angular.module('root-app', [])
+
+  .controller('ModalCtrl', function () {
+    let vm = this;
+    this.btnClick = function (tab) {
+      if (tab === 'records') {
+        vm.recordActive = true;
+        vm.title = 'Records';
+        vm.name = 'Record';
+        vm.verb = 'Recorded';
+      }
+      else {
+        vm.plansActive = true;
+        vm.title = 'Plans';
+        vm.name = 'Plan';
+        vm.verb = 'Planned';
+      }
+    };
+  })
+  .directive('modalForm', function () {
+    return {
+      restrict: 'E',
+      templateUrl: 'modalForm.html'
+    };
+  });
+
+
+
+
