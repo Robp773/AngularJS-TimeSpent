b0ba3eb1220fb851308a84e799f74c5f1084766e
Robp773 <robpeterman573@gmail.com>
2018-07-07 17:23:19 -0400

    added basic time calculations and partial recorded/planned form functionality

--- app/home/home.js
+++ app/home/home.js
@@ -1,10 +1,58 @@
 'use strict';
 
 angular.module('root-app', [])
+  .factory('totals', function () {
+    let totals = {};
+    let newTime = new Date();
+    let passedMins = newTime.getHours() * 60 + newTime.getMinutes();
 
+    // MOCK DATA - eventually make GET requests to get initial values
+    // -----------------------
+    totals.plannedMins = 14;
+    totals.recordedMins = 45;
+    totals.unspentPastMins = passedMins - totals.recordedMins;
+    totals.timeLeft = 1440 - passedMins - totals.plannedMins;
+    // -----------------------
 
+    // update the number of passed minutes 
+    totals.timeTracker = function () {
+      let newTime = new Date();
+      passedMins = newTime.getHours() * 60 + newTime.getMinutes();
+      totals.unspentPastMins = passedMins - totals.recordedMins;
+      totals.timeLeft = 1440 - passedMins - totals.plannedMins;
+    };
 
-  .controller('ModalCtrl', function () {
+    // POST requests - send POST reqs to update DB and then update local values to reflect changes (totals.passedMins/timeLeft)
+    totals.updateRecorded = function (newMins) {
+
+      // deletions to record list
+      if (newMins < 0) {
+        totals.recordedMins = totals.recordedMins - newMins;
+        totals.unspentPastMins = totals.unspentPastMins + newMins;
+      }
+      // additions to record list
+      else {
+        totals.recordedMins = totals.recordedMins + newMins;
+        totals.unspentPastMins = totals.unspentPastMins - newMins;
+      }
+    };
+
+    totals.updatePlanned = function (newMins) {
+
+      // deletions to planned list
+      if (newMins < 0) {
+        totals.plannedMins = totals.plannedMins - newMins;
+        totals.timeLeft = totals.timeLeft + newMins;
+      }
+      // additions to planned list
+      else {
+        totals.plannedMins = totals.plannedMins + newMins;
+        totals.timeLeft = totals.timeLeft - newMins;
+      }
+    };
+    return totals;
+  })
+  .controller('ModalCtrl', function (totals) {
     let vm = this;
     this.btnClick = function (tab) {
       if (tab === 'records') {
@@ -25,15 +73,39 @@ angular.module('root-app', [])
       vm.plansActive = false;
       document.getElementById('modalForm').reset();
     };
+
+    this.formSubmit = function (title, minutes) {
+      if (title === 'Recorded') {
+        totals.updateRecorded(minutes);
+      }
+      else {
+        totals.updatePlanned(minutes);
+      }
+      document.getElementById('modalForm').reset();
+    };
   })
 
-  .controller('BannerCtrl', function ($interval, $rootScope) {
+  .controller('BannerCtrl', function ($interval) {
     let vm = this;
     vm.date = new Date();
-
     $interval(function () {
       vm.date = new Date();
     }, 1000);
+  })
+
+  .controller('TotalsCtrl', function ($interval, totals) {
+    let vm = this;
+    vm.data = totals;
+
+    // this.test = function (mins) {
+    //   totals.updateRecorded(mins);
+    //   totals.updatePlanned(mins);
+    // };
+
+    $interval(function () {
+      totals.timeTracker();
+    }, 1000);
+
   })
 
   .directive('modalForm', function () {
@@ -42,8 +114,3 @@ angular.module('root-app', [])
       templateUrl: 'modalForm.html'
     };
   });
-
-
-
-
-
